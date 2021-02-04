816f2eed4a08db1856702e89beb92dcb15993870
KeenWarrior <anujgargcse@gmail.com>
2021-02-04 16:27:42 +0530

    L2 + L3

---
+++ basicloops.js
@@ -0,0 +1,15 @@
+let reps = 0;
+
+
+while(reps < 5){
+    
+    let clap = 0;
+    while(clap < 5){
+        process.stdout.write("* ");
+        clap += 1;
+    }
+    process.stdout.write("\n");
+
+    reps += 1;
+}
+
---
+++ downtrig.js
@@ -0,0 +1,14 @@
+let n = 6;
+
+let row = 0;
+while(row < n){
+    let col = 0;
+    while(col < n-row){
+        process.stdout.write("* ");
+        col += 1;
+    }
+    process.stdout.write("\n");
+
+    row += 1;
+}
+
