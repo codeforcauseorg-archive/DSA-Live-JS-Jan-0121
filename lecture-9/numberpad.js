a3e1b1a41cb1d295b926e6f28993db69dbc91d11
KeenWarrior <anujgargcse@gmail.com>
2021-02-13 17:22:11 +0530

    L 9

--- lecture-9/numberpad.js
+++ lecture-9/numberpad.js
@@ -4,4 +4,20 @@ function getKeyLetter(key){
     return [...letters[key]];
 }
 
-console.log(getKeyLetter(2));
+function numpad(unproc, proc=""){
+    if(unproc == ""){
+        console.log(proc);
+        return;
+    }
+
+    let key = parseInt(unproc[0]);
+    let letters = getKeyLetter(key);
+
+    let mod_unproc = unproc.substring(1)
+    for (const letter of letters) {
+        numpad(mod_unproc, proc + letter);
+    }
+}
+
+// console.log(getKeyLetter(2));
+numpad("24");
